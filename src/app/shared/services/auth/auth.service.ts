cd1fb94a50063c1bd99d76be8023ca95f28aa2f1
Alexander Targov <alexander.targov@synchronoss.com>
2020-06-18 00:46:44 +0300

    feat: geeet user by token

--- src/app/shared/services/auth/auth.service.ts
+++ src/app/shared/services/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { Observable } from 'rxjs';
 
 import { ApiService } from '../api/api.service';
@@ -8,7 +9,10 @@ import { IUser } from './user.interface';
 export class AuthService  {
     public readonly route = '/user';
 
-    constructor(private readonly apiService: ApiService) {
+    constructor(
+        private readonly apiService: ApiService,
+        private readonly jwtService: JwtHelperService,
+    ) {
     }
 
     public login(user: IUser): Observable<{ token: string }> {
@@ -21,11 +25,21 @@ export class AuthService  {
     public register(user: IUser): Observable<void> {
         return this.apiService.request(`${this.route}/register`, {
             method: 'post',
-            user
+            data: user
         });
     }
 
     public getToken(): string {
         return sessionStorage.getItem('access_token');
+    }
+
+    public getUser(): IUser {
+        const token = this.getToken();
+
+        if (!token) {
+            return null;
+        }
+
+        return this.jwtService.decodeToken(token);
     }
 }
