c8228c4e1af4a10e95c9f4e5cc2435c34e895c73
Alexander Targov <alexander.targov@synchronoss.com>
2020-07-01 15:44:23 +0300

    feat: data reshresh changes

--- src/app/shared/components/data/data.component.ts
+++ src/app/shared/components/data/data.component.ts
@@ -1,13 +1,13 @@
-import { Component, Input, ViewChild, AfterViewInit, OnChanges, OnInit, SimpleChanges, NgZone } from '@angular/core';
+import { Component, Input, ViewChild, AfterViewInit, OnChanges, OnInit, SimpleChanges, NgZone, SimpleChange } from '@angular/core';
 
 import { IVoltage } from '../../interfaces/voltage.interface';
 import { ICurrent } from '../../interfaces/current.interface';
 import { IConsumption } from '../../interfaces/consumption.interface';
 import { ITimeFrame } from '../../services/timeFrame/time-frame.interface';
 import { TimeFrames } from '../../services/timeFrame/time-frames.enum';
-
 import { TimeFrameService } from '../../services/timeFrame/time-frame.service';
-import { ChartService } from '../../services/chart/chart.service';
+import { BroadcasterService } from '../../services/broadcaster/broadcaster.service';
+import { broadcasterEvents } from '../../constants/broadcaster-event.constants';
 
 
 import * as am4core from '@amcharts/amcharts4/core';
@@ -16,7 +16,6 @@ import am4themes_animated from '@amcharts/amcharts4/themes/animated';
 am4core.options.queue = true;
 am4core.options.minPolylineStep = 5;
 am4core.options.animationsEnabled = false;
-
 am4core.useTheme(am4themes_animated);
 
 @Component({
@@ -37,6 +36,7 @@ export class DataComponent implements OnInit, AfterViewInit, OnChanges {
 
     constructor(
         private readonly timeFrameService: TimeFrameService,
+        private readonly broadcasterService: BroadcasterService,
         private readonly zone: NgZone,
     ) {}
 
@@ -47,10 +47,59 @@ export class DataComponent implements OnInit, AfterViewInit, OnChanges {
     }
 
     public ngAfterViewInit(): void {
+        this.initializeChart(this.data);
+
+        this.chart.events.on('datavalidated', () => {
+            this.broadcasterService.broadcast(broadcasterEvents.chartRendered, this.chart.uid);
+        });
+
+        this.chart.events.on('beforedisposed', () => {
+            this.broadcasterService.broadcast(broadcasterEvents.chartRendered, this.chart.uid);
+        });
+    }
+
+    public async ngOnChanges({ timeFrame, data}: SimpleChanges): Promise<void> {
+        if (timeFrame) {
+            await this.getTimeFrames();
+
+            // this.chart.categoryAxis.minPeriod = this.chartService.getChartPeriods(this.timeFrame, this.timeFrameRanges).minPeriod;
+            // console.log(this.chart.categoryAxis.minPeriod);
+        }
+
+        if (data && this.chart && this.timeFrame) {
+            this.onDataChange(this.data, this.chart);
+        }
+
+    }
+
+    private onDataChange(data: IConsumption[] | ICurrent[] | IVoltage [], chart: am4charts.XYChart): void {
+        if (this.timeFrame.frame === TimeFrames.todayLive && data.length && data.length <= 2) {
+            chart.addData(data, data.length);
+            return;
+        }
+
+        if (this.timeFrame.frame === TimeFrames.todayLive && data.length > 2) {
+            chart.data = data;
+            return;
+        }
+
+        if (this.timeFrame.frame === TimeFrames.todayPartly) {
+            chart.dateFormatter.timezoneOffset = 0;
+        }
+
+        this.chart.data = data;
+    }
+
+    private async getTimeFrames(): Promise<void> {
+        this.timeFrameRanges = await this.timeFrameService.getTimeFrame(this.timeFrame).toPromise();
+    }
+
+    private initializeChart(data): void {
         this.zone.runOutsideAngular(() => {
             const chart = am4core.create(this.title, am4charts.XYChart);
-
-            chart.data = this.data;
+            chart.dateFormatter = new am4core.DateFormatter();
+            chart.dateFormatter.utc = false;
+            chart.data = data;
 
             const dateAxis = chart.xAxes.push(new am4charts.DateAxis());
             dateAxis.renderer.grid.template.location = 0;
@@ -71,33 +120,11 @@ export class DataComponent implements OnInit, AfterViewInit, OnChanges {
             series.tooltipText = '{valueY.value}';
             chart.cursor = new am4charts.XYCursor();
 
-            const scrollbarX = new am4charts.XYChartScrollbar();
-            scrollbarX.series.push(series);
-            chart.scrollbarX = scrollbarX;
+            // const scrollbarX = new am4charts.XYChartScrollbar();
+            // scrollbarX.series.push(series);
+            // scrollbarX.series.clear();
+            // chart.scrollbarX = scrollbarX;
             this.chart = chart;
         });
-    }
-
-    public async ngOnChanges(change: SimpleChanges): Promise<void> {
-        if (change.timeFrame) {
-            await this.getTimeFrames();
-
-            // this.chart.categoryAxis.minPeriod = this.chartService.getChartPeriods(this.timeFrame, this.timeFrameRanges).minPeriod;
-            // console.log(this.chart.categoryAxis.minPeriod);
-        }
-
-        if (change.data && this.chart && this.timeFrame) {
-            this.onDataChange(this.data, this.chart);
-        }
-    }
-
-    private onDataChange(data: IConsumption[] | ICurrent[] | IVoltage [], chart: am4charts.XYChart): void {
-        if (this.timeFrame.frame === TimeFrames.today) {
-            chart.addData(data, 0);
-        }
-    }
-
-    private async getTimeFrames(): Promise<void> {
-        this.timeFrameRanges = await this.timeFrameService.getTimeFrame(this.timeFrame).toPromise();
     }
 }
